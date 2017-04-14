63ed05fb1961246fe3f05ed5f59278607387a709
Reinforce-II <fate@eastal.com>
2017-04-14 16:08:08 +0800

    init

--- test.sh
+++ test.sh
@@ -8,6 +8,6 @@ if [ -f /tmp/test.log ]; then
     rm /tmp/test.log
 fi
 
-parallel 'timeout 2 curl -o /dev/null {} -w "%{speed_download}" 2> /dev/null; echo " {}"; ' >> /tmp/test.log ::: `cat testlist.txt`
+parallel 'timeout 2 curl -o /dev/null {}core/os/x86_64/core.db -w "%{speed_download}" 2> /dev/null; echo " {}"; ' >> /tmp/test.log ::: `cat testlist.txt`
 
-cat /tmp/test.log|grep '[000]'|sort -nr|head -n$num|awk '{print $2}'
+cat /tmp/test.log|grep '[000]'|sort -nr|head -n$num|awk '{print $2 "\$repo/os/\$arch"}' 2> /dev/null
