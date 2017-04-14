aeb7048c74ebd7afd599995d9214d8b3386726be
Reinforce-II <fate@eastal.com>
2017-04-14 16:00:50 +0800

    init

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+num=5
+if [ ! -z $1 ]; then
+num=$1
+fi
 echo Please wait...
 if [ -f /tmp/test.log ]; then
     rm /tmp/test.log
@@ -6,5 +10,4 @@ fi
 
 parallel 'timeout 2 curl -o /dev/null {} -w "%{speed_download}" 2> /dev/null; echo " {}"; ' >> /tmp/test.log ::: `cat testlist.txt`
 
-cat /tmp/test.log|grep '[000]'|sort -nr|head -n5|awk '{print $2}'
- 
+cat /tmp/test.log|grep '[000]'|sort -nr|head -n$num|awk '{print $2}'
